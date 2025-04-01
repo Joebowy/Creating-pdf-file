4ba40f1ee89b94a2e735aaa0421c002998b3cf44
joeseph <otoojoe230@gmail.com>
2025-04-01 19:25:26 +0200

    Adding pages and footer

--- creatingPdf.py
+++ creatingPdf.py
@@ -6,14 +6,31 @@ df=pd.read_csv("topics.csv")
 
 # generating pdf file
 pdf=FPDF(orientation="P", unit="mm",format="A4")
+pdf.set_auto_page_break(auto=False)
 
 # iterating over the rows
 for index, row in df.iterrows():
 
     pdf.add_page()
+    # set a header
     pdf.set_font(family="Times",size=24, style="B")
-    pdf.cell(w=0,h=12, txt=row["Topic"],ln=1,border=0)
+    pdf.cell(w=0,h=12, txt=row["Topic"],ln=1,border=0,align="L")
     pdf.set_text_color(100,100,100)
     pdf.line(10,21,200,21)
+
+    # set a footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", size=20, style="I")
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, border=0,align="R")
+    pdf.set_text_color(180, 180, 180)
+
+    # adding pages
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        pdf.ln(275)
+        pdf.set_font(family="Times", size=20, style="I")
+        pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, border=0, align="R")
+        pdf.set_text_color(180, 180, 180)
+
 
 pdf.output("output.pdf")
